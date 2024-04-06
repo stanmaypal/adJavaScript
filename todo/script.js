aa53be80f7ad2d0a986cfbfddbe89b7e2d6ee185
stanmaypal <tanmay0pal@gmail.com>
2024-04-06 15:48:00 +0530

    Local Storage 06-04

--- todo/script.js
+++ todo/script.js
@@ -1,6 +1,13 @@
 const item=document.querySelector('#txt');//get input data
 const list_item=document.querySelector('#list');//get list area
 
+document.addEventListener('DOMContentLoaded',function(){
+
+const saveItem=JSON.parse(localStorage.getItem('newItems'))||[]
+
+saveItem.forEach(items=>adddata(items));
+})
+
             item.addEventListener('keyup',function(add){
             //console.log(add.key);
             if(add.key=='Enter')
@@ -11,26 +18,31 @@ const list_item=document.querySelector('#list');//get list area
             }
             })
 
-
-
-
-
 function adddata(data)
 {
     const newItem=document.createElement('li');
     newItem.innerHTML=`${data}
     <i class="fa fa-trash-o"></i>
     `
+    // updatelocalstorage()
 newItem.addEventListener("click",function(){
     newItem.classList.toggle('cmptsk')
+    //sendStorage()
 })
 
 newItem.querySelector('i').addEventListener('click',function(){
 
     newItem.remove();
+    //sendStorage()
 })
 
 
 
 list_item.appendChild(newItem)
+updatelocalstorage()
+}
+function updatelocalstorage()
+{
+    const items=Array.from(list_item.children).map(child=>child.textContent.trim())
+    localStorage.setItem('newItems',JSON.stringify(items))
 }
